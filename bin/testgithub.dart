38b0c2f827f2057f00906f42c9ba6ac15957de80
escribanoVegaJose <escribanovegajose@gmail.com>
2020-10-22 10:15:00 +0200

    feat: [FUNC][JIRA]- added resta func

--- bin/testgithub.dart
+++ bin/testgithub.dart
@@ -3,3 +3,7 @@ import 'package:testgithub/testgithub.dart' as testgithub;
 void main(List<String> arguments) {
   print('Hello world: ${testgithub.calculate()}!');
 }
+
+int resta(int num1, int num2) {
+  return num1 - num2;
+}
