a1138501465e0b973939ab13a55416bc6f9763ca
escribanoVegaJose <escribanovegajose@gmail.com>
2020-10-22 11:12:42 +0200

    feat: [FUNC][JIRA]- added multiplacr func

--- bin/testgithub.dart
+++ bin/testgithub.dart
@@ -3,3 +3,7 @@ import 'package:testgithub/testgithub.dart' as testgithub;
 void main(List<String> arguments) {
   print('Hello world: ${testgithub.calculate()}!');
 }
+
+double multiplicar(double num1, double num2) {
+  return num1 * num2;
+}
